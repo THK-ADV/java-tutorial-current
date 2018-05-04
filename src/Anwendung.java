b68556b2bc4700dd91c47fa1c13efa0d325a90be
Florian Herborn <florian.herborn@th-koeln.de>
2018-05-04 11:22:52 +0200

    Added Java-Doc

--- src/Anwendung.java
+++ src/Anwendung.java
@@ -1,5 +1,9 @@
 public class Anwendung {
 
+    /**
+     * Einstiegspunkt des Programms
+     * @param args Argumente die beim Start des Programms übergeben werden können
+     */
     public static void main(String[] args) {
 
     }
